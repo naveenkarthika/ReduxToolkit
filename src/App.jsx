73a20f316e3f97a3c2e4605a6238fb78856c455e
naveenkarthika <yuvannaveen65@gmail.com>
2022-05-15 19:05:43 +0530

    code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
-import logo from './logo.svg'
 import './App.css'
 import Icecream from './app/container/Icecream'
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const dispatch = useDispatch();
-
-  const value = useSelector(state => state)
-  console.log('value',value);
-
   return (
     <div className="App">
       <p>Test 24324</p>
